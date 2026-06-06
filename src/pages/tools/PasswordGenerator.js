b40e23e4114ee24999f7afd7b8122c015c4efbf2
Ranjith Kumar <ranjithkumarsaravanandev@gmail.com>
2026-06-06 17:11:52 +0530

    Added the Password Generator

---
+++ src/pages/tools/PasswordGenerator.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PasswordGenerator = () => {
+    return (
+        <div>
+            This is password Generator
+        </div>
+    )
+}
+
+export default PasswordGenerator;
